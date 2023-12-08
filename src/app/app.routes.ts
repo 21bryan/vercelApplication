97c733dba1a295723297e81001f4810e9c463b31
21bryan <bryanbeauclaire@gmail.com>
2023-12-08 13:42:11 +0100

    feat depot de mon application vercel

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+
+export const routes: Routes = [
+    
+    {path:"", component:ContentComponent},
+
+];
